2bf6f0d8a4f433ec7590117af1e2fc10f69d0f23
sougajam <bidya9035@gmail.com>
2026-08-26 09:37:01 +0530

    Updated frontend to use live Render API

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -14,6 +14,7 @@ export function Articles() {
     async function fetchArticles() {
       try {
         setIsLoading(true);
+        console.log("My Live API URL is:", import.meta.env.VITE_API_URL);
         const response = await fetch(
           `${import.meta.env.VITE_API_URL}/api/articles`,
         );
